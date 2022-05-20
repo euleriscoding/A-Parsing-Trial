54de3f585624ec21ed3eb418add92d81b970be22
euleriscoding <101626977+euleriscoding@users.noreply.github.com>
2022-05-20 19:56:02 +0300

    Add files via upload

---
+++ lexing.py
@@ -0,0 +1,59 @@
+import lex
+# List of token names
+tokens = (
+    'NUMBER',
+    'RED',
+    'GREEN',
+    'BLUE',
+    'BLACK',
+    'FORW',
+    'RGHT',
+    'LOOP',
+    'COLOR',
+    'PEN',
+    'LSQB',
+    'RSQB',
+    'EMPTY'
+) 
+# Regular expression rules for simple tokens
+t_FORW    = r'F'
+t_RGHT   = r'R'
+t_LOOP   = r'L'
+t_COLOR  = r'COLOR'
+t_PEN  = r'PEN'
+t_LSQB  = r'\['
+t_RSQB  = r'\]'
+t_RED  = r'K'
+t_GREEN  = r'Y'
+t_BLUE  = r'M'
+t_BLACK  = r'S'
+t_EMPTY = r'\ ' 
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)    
+    return t 
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value) 
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t' 
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1) 
+# Build the lexer
+lexer = lex.lex()
+# Test it out
+data = '''
+L 36 [L 4 [F 100 R 90] R 10]
+''' 
+# Give the lexer some input
+lexer.input(data)
+# Tokenize 
+for tok in lexer:
+    print(tok)
+
+
+
