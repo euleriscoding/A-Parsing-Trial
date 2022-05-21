e4dfbe6748b920e5fe6d3dd6f1994e6edfe6cb6e
euleriscoding <101626977+euleriscoding@users.noreply.github.com>
2022-05-21 15:18:24 +0300

    Update lexing.py

--- lexing.py
+++ lexing.py
@@ -27,16 +27,15 @@ t_GREEN  = r'Y'
 t_BLUE  = r'M'
 t_BLACK  = r'S'
  
-# A regular expression rule with some action code
 def t_NUMBER(t):
     r'\d+'
     t.value = int(t.value)    
     return t 
-# Define a rule so we can track line numbers
+
 def t_newline(t):
     r'\n+'
     t.lexer.lineno += len(t.value) 
-# A string containing ignored characters (spaces and tabs)
+    
 t_ignore  = ' \t' 
 # Error handling rule
 def t_error(t):
@@ -48,7 +47,7 @@ lexer = lex.lex()
 data = '''
 F 50 R 90
 ''' 
-# Give the lexer some input
+
 lexer.input(data)
 # Tokenize 
 for tok in lexer:
